b02a30b2a4ab57e37dafbf2ee9fe21a49ee86a4d
Saad Kureshi <saq619@gmail.com>
2020-12-08 20:27:38 +0000

    Updated

--- express_server.js
+++ express_server.js
@@ -40,6 +40,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL;
+  res.redirect("/urls")
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
